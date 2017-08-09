ff2ae99a2a3056ddb70a585a97b247c8c6fde554
harrison-symes <symeshjb@gmail.com>
2017-08-10 11:43:36 +1200

    there was a bug, silly me

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ var createServer = require('./server')
 
 var Knex = require('knex')
 var config = require('../knexfile')[process.env.NODE_ENV || 'development']
-var knex = knex(config)
+var knex = Knex(config)
 
-var server = createServer(knex
+var server = createServer(knex)
 var PORT = process.env.PORT || 3000
 
 server.listen(PORT, function () {
