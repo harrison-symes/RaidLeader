7d737e4bb1093ce14ebc0942f989879a2005dc18
harrison-symes <symeshjb@gmail.com>
2017-08-29 05:27:05 +1200

    login route working and handling error cases

--- server/auth/token.js
+++ server/auth/token.js
@@ -1,14 +1,22 @@
 var jwt = require('jsonwebtoken')
 var {getUserByName} = require('../db/users')
 var verifyJwt = require('express-jwt')
+var {compare} = require('./hash')
 
 function issue (req, res) {
+  console.log(req.body);
   getUserByName(req.body.user_name, req.app.get('db'))
     .then(user => {
-      var token = createToken(user, process.env.JWT_SECRET)
-      res.json({
-        message: 'Authentication successful',
-        token
+      compare(req.body.password, user.hash, (err, match) => {
+        if (err) res.status(500).json({message: err.message})
+        else if (!match) res.status(400).json({message: 'password is incorrect'})
+        else {
+          var token = createToken(user, process.env.JWT_SECRET)
+          res.json({
+            message: 'Authentication successful',
+            token
+          })
+        }
       })
     })
 }
