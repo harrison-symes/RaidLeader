58c50673f50b49d5742da2f8e30ca7562eec9cac
harrison-symes <symeshjb@gmail.com>
2018-01-06 19:28:15 +1300

    Mage trait implemented

--- client/components/classes/Mage.jsx
+++ client/components/classes/Mage.jsx
@@ -3,20 +3,27 @@ import PartyMemberFrame from '../frames/PartyMemberFrame'
 import {connect} from 'react-redux'
 
 class Mage extends PartyMemberFrame {
-  startFighting () {
+  finishCast(power) {
+    if (this.props.player.mana / this.props.player.maxMana * 100 < 30) power*2
+    this.props.dispatch({type: 'SPECIAL_ATTACK_BOSS', power})
+    this.startCast()
+  }
+  startCast() {
     const {power, speed} = this.props.member
+    setTimeout(() => this.finishCast(power), 10000 / speed)
+  }
+  startFighting () {
     this.props.dispatch({type: 'MAGE_START_BUFF'})
-    let interval = null
-    interval = setInterval(() => this.specialAttack(power), 10000 / speed)
-    this.setState({interval})
+    this.startCast()
   }
 }
 
-const mapStateToProps = ({started, party, friendlyTarget}) => {
+const mapStateToProps = ({started, party, player, friendlyTarget}) => {
   return {
     started,
     party,
-    friendlyTarget
+    friendlyTarget,
+    player
   }
 }
 
