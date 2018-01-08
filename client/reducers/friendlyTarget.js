7ad5ff84fb54f735c3040e3d4e87e21305936e8c
harrison-symes <symeshjb@gmail.com>
2018-01-08 16:23:07 +1300

    targetting is cancelled when a friendly target dies

--- client/reducers/friendlyTarget.js
+++ client/reducers/friendlyTarget.js
@@ -4,6 +4,8 @@ export default function (state = null, action) {
     case 'SELECT_FRIENDLY_TARGET':
       if (action.target.isAlive) return action.target
       else return state
+    case 'MEMBER_DIED':
+      if (action.target == state) return null
     default:
       return state
   }
