41987d8aa4d23f0844526a1a86523f6ee8b23f35
harrison-symes <symeshjb@gmail.com>
2018-01-09 16:05:27 +1300

    drag and drop party and recruits frames displaying

--- client/components/menuComponents/Party.jsx
+++ client/components/menuComponents/Party.jsx
@@ -1,30 +1,102 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+
+const grid = 8;
+const getItemStyle = (draggableStyle, isDragging) => ({
+  userSelect: 'none',
+  padding: grid * 2,
+  margin: `0 0 ${grid}px `,
+  background: isDragging ? 'lightgreen' : 'grey',
+  ...draggableStyle,
+});
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  padding: grid,
+  width: 250,
+  height: 250
+});
+
 class Party extends React.Component {
   constructor(props) {
     super(props)
-
+    this.onDragEnd = this.onDragEnd.bind(this);
   }
   moveToParty(recruit) {
     this.props.dispatch({type: 'ADD_RECRUIT_TO_PARTY', recruit})
   }
+  onDragEnd(result) {
+    console.log({result});
+  }
   render() {
     console.log(this.props);
     const {recruits, playerParty} = this.props
-    return <div className="columns">
-      <div className="column is-6">
-        <h1 className="title is-2">Recruits</h1>
-        {recruits.map((recruit, i) => {
-          return <div onClick={() => this.moveToParty(recruit)} className="box is-large">{recruit.name}</div>
-        })}
-      </div>
-      <div className="column is-6">
-        <h1 className="title is-2">Party</h1>
-        {playerParty.map((recruit, i) => {
-          return <div className="tag is-large">{recruit.name}</div>
-        })}
-      </div>
+    return <div className="has-text-centered">
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <div className="columns">
+          <Droppable droppableId="recruits">
+            {(provided, snapshot) => (
+              <div
+                className="Recruits"
+                ref={provided.innerRef}
+                style={getListStyle(snapshot.isDraggingOver)}
+                >
+                {recruits.map(item => (
+                  <Draggable key={item.id} draggableId={item.id}>
+                    {(provided, snapshot) => (
+                      <div>
+                        <div
+                          ref={provided.innerRef}
+                          style={getItemStyle(
+                            provided.draggableStyle,
+                            snapshot.isDragging
+                          )}
+                          {...provided.dragHandleProps}
+                          >
+                          {item.name}
+                        </div>
+                        {provided.placeholder}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+              )}
+          </Droppable>
+          <Droppable droppableId="party">
+            {(provided, snapshot) => (
+              <div
+                className="Party"
+                ref={provided.innerRef}
+                style={getListStyle(snapshot.isDraggingOver)}
+                >
+                {playerParty.map(item => (
+                  <Draggable key={item.id + '-party'} draggableId={item.id + '-party'}>
+                    {(provided, snapshot) => (
+                      <div>
+                        <div
+                          ref={provided.innerRef}
+                          style={getItemStyle(
+                            provided.draggableStyle,
+                            snapshot.isDragging
+                          )}
+                          {...provided.dragHandleProps}
+                          >
+                          {item.name}
+                        </div>
+                        {provided.placeholder}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+              )}
+          </Droppable>
+        </div>
+      </DragDropContext>
     </div>
   }
 }
--- client/reducers/playerParty.js
+++ client/reducers/playerParty.js
@@ -2,7 +2,8 @@ export default function (state = [], action) {
   switch(action.type) {
     case 'ADD_RECRUIT_TO_PARTY':
       console.log({action});
-      return [...state, action.recruit]
+      if (state.find(recruit => recruit == action.recruit)) return [...state].filter(recruit => recruit != action.recruit)
+      else return [...state, action.recruit]
     default: return state
   }
 }
