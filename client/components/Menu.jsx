a6bd64fadf4e8756e63f30211941aee22ae9cf23
harrison-symes <symeshjb@gmail.com>
2018-01-11 06:55:04 +1300

    your party is displayed on the main menu screen now

--- client/components/Menu.jsx
+++ client/components/Menu.jsx
@@ -22,14 +22,22 @@ class Menu extends React.Component {
     this.props.dispatch({type: 'LOAD_GAME', playerParty})
   }
   render() {
+    const {playerParty} = this.props
     return <div className="section has-text-centered">
       <div className="columns">
         <div className="column is-6">
-          <Link className="button is-large" to="/spellbook">Spell Book</Link>
-          <Link className="button is-large" to="/party">Party</Link>
-          <Link className="button is-large" to="/selectboss">Select Boss</Link>
-          <Link className="button is-large is-info" onClick={this.loadGame} to="/game">Start Game</Link>
-          {/* <Link className="button is-large" to="/inventory">Inventory</Link> */}
+          <div className="level">
+            <Link className="button is-large" to="/spellbook">Spell Book</Link>
+            <Link className="button is-large" to="/party">Party</Link>
+            <Link className="button is-large" to="/selectboss">Select Boss</Link>
+            <Link className="button is-large is-info" onClick={this.loadGame} to="/game">Start Game</Link>
+          </div>
+          <div className="has-text-centered">
+            <p className="subtitle is-1">Your Party:</p>
+            <div className="columns">
+              {playerParty.map(({name, heroClass}, i) => <div key={`player-in-party-main-${i}`} className="tag is-info is-large">{name} the {heroClass}</div>)}
+            </div>
+          </div>
         </div>
         <div className="column is-6 box">
           <Router>
