71d77e6e15331a87cf4dd9b5a69b6829956fbad4
harrison-symes <symeshjb@gmail.com>
2018-01-14 06:52:43 +1300

    dungeon progress deleted if refreshed during fight

--- client/reducers/location.js
+++ client/reducers/location.js
@@ -2,17 +2,24 @@ import {set, get} from '../utils/localstorage'
 
 const town = {
   name: 'Town',
-  type: 'Town'
+  type: 'Town',
+  inGame: false
 }
 
 let initialState = JSON.parse(get('location'))
+if (initialState.inGame) initialState = null
 
 export default function (state = initialState || town, action) {
   let newState = {...state}
   switch (action.type) {
+    case 'START':
+      newState.inGame = true
+      set('location', JSON.stringify(newState))
+      return newState
     case 'GAME_WON':
       let boss = newState.bosses.find(dungeonBoss => dungeonBoss.name == action.boss.name)
       boss.isDefeated = true
+      newState.inGame = false
       set('location', JSON.stringify(newState))
       return newState
     case 'TRAVEL_TO_DUNGEON':
@@ -20,6 +27,7 @@ export default function (state = initialState || town, action) {
         boss.isDefeated = false
         return boss
       })
+      action.dungeon.inGame = false
       set('location', JSON.stringify(action.dungeon))
       return action.dungeon
     case 'TRAVEL_TO_TOWN':
