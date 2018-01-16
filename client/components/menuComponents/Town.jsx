4dc0a784a0aa56c0c01785d4b261d771e40a3e43
harrison-symes <symeshjb@gmail.com>
2018-01-17 10:16:47 +1300

    your gold is displayed on the town screen

--- client/components/menuComponents/Town.jsx
+++ client/components/menuComponents/Town.jsx
@@ -38,9 +38,12 @@ class Town extends Component {
     const {gold} = this.props
     const {showRecruitmentModal} = this.state
     return <div style={{height: '89vh', margin: '0'}} className="hero is-bold is-info">
-      {/* <div className="hero-head has-text-centered">
-      </div> */}
-      <p className="title is-1">Town</p>
+      <div className="hero-head has-text-centered">
+        <div className="">
+          <p className="title is-1">Town</p>
+        </div>
+        <p className="subtitle is-2">Gold: {gold}</p>
+      </div>
       {this.modalSwitch()}
       <div className="hero-body is-fullheight has-text-centered">
         <Link to="/dungeons" style={{height: '15vh', width: '45vw', margin: 'auto', marginBottom: '2vw'}} className="button is-large is-fullwidth is-outlined is-warning">Travel</Link>
