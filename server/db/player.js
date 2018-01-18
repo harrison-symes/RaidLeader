720784ee72eadafc90998c588fad4f0b9dc6feeb
harrison-symes <symeshjb@gmail.com>
2018-01-18 15:46:44 +1300

    stopped weapons from other players showing up

--- server/db/player.js
+++ server/db/player.js
@@ -6,5 +6,5 @@ module.exports = {
   getPlayerGold: (user_id) => knex.select('gold').from('users').where({user_id}).first(),
   updatePlayerGold: (user_id, gold) => knex('users').update({gold}).where({user_id}),
   addWeapon: (user_id, name, level) => knex('inventory').insert({user_id, name, level, is_weapon: true}),
-  getWeapons: user_id => knex('inventory').where('is_weapon', true)
+  getWeapons: user_id => knex('inventory').where({is_weapon: true, user_id})
 }
