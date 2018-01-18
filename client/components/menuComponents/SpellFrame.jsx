487480bc15bae49f549ff69c6f99bdda16dd583f
harrison-symes <symeshjb@gmail.com>
2018-01-18 15:58:20 +1300

    spell preview modal with descriptions on spellbook section

--- client/components/menuComponents/SpellFrame.jsx
+++ client/components/menuComponents/SpellFrame.jsx
@@ -15,83 +15,33 @@ class SpellFrame extends React.Component {
   render() {
     const {spell} = this.props
     const {showMore} = this.state
-    return <tbody className="tbody">
-      <tr className="tr">
-        <th className="th">
-          <p className="subtitle is-4">Spell:</p>
-        </th>
-        <td className="td">
-          <p className="subtitle is-4">
-            {spell.name}
-          </p>
-        </td>
-      </tr>
-      {showMore && [
-        (<tr key={`spell-${name}-hp`} className="tr">
-          <th className="th">
-            <p className="subtitle is-4">Type:</p>
-          </th>
-          <td className="td">
-            <p className="subtitle is-4">
-              {spell.type}
-            </p>
-          </td>
-        </tr>),
-        (<tr key={`spell-${name}-hp`} className="tr">
-          <th className="th">
-            <p className="subtitle is-4">Targeted:</p>
-          </th>
-          <td className="td">
-            <p className="subtitle is-4">
-              {spell.singleTarget ? "Yes" : "No"}
-            </p>
-          </td>
-        </tr>),
-        (<tr key={`spell-${name}-cooldown`} className="tr">
-          <th className="th">
-            <p className="subtitle is-4">CD:</p>
-          </th>
-          <td className="td">
-            <p className="subtitle is-4">
-              {spell.coolDown} s
-            </p>
-          </td>
-        </tr>),
-        (<tr key={`spell-${name}-power`} className="tr">
-          <th className="th">
-            <p className="subtitle is-4">Power:</p>
-          </th>
-          <td className="td">
-            <p className="subtitle is-4">
-              {spell.powerRatio * 100} %
-            </p>
-          </td>
-        </tr>),
-        (<tr className="tr">
-          <th className="th">
-            <p className="subtitle is-4">Cost:</p>
-          </th>
-          <td className="td">
-            <p className="subtitle is-4">
-              {spell.cost} mana
-            </p>
-          </td>
-        </tr>),
-        (<tr className="tr">
-          <th className="th">
-            <p className="subtitle is-4">Cast:</p>
-          </th>
-          <td className="td">
-            <p className="subtitle is-4">
-              {spell.cast} s
-            </p>
-          </td>
-        </tr>)
-      ]}
-      <tr className="tr has-text-centered">
-        <button onClick={this.toggleShow} className="button">{showMore ? 'Show Less' : 'Show More'}</button>
-      </tr>
-    </tbody>
+    const modal = () => <div className="modal is-active">
+      <div className="modal-background"></div>
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <p className="modal-card-title">{spell.name}</p>
+          <button onClick={this.toggleShow} className="delete" aria-label="close"></button>
+        </header>
+        <div className="modal-card-body has-text-centered">
+          <p className="title is-1">{spell.name} ({spell.type})</p>
+          <hr />
+          <div className="columns">
+            <div className="column is-4"><p className="subtitle is-3">Cost: {spell.cost} mana</p></div>
+            <div className="column is-4"><p className="subtitle is-3">Cast: {spell.cast} s</p></div>
+            <div className="column is-4"><p className="subtitle is-3">CD: {spell.coolDown} s</p></div>
+          </div>
+          <p className="box subtitle is-2">{spell.description}</p>
+        </div>
+        <footer className="modal-card-footer">
+          <button onClick={this.toggleShow} className="modal-close is-large" aria-label="close">Close</button>
+        </footer>
+      </div>
+    </div>
+    return <div>
+      <p className="subtitle is-4">{spell.name}</p>
+      <button onClick={this.toggleShow} className="button">Show More</button>
+      {showMore && modal()}
+    </div>
   }
 }
 
