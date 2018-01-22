559dc5f2c6941cfea5af64be1b1293d23bb76469
harrison-symes <symeshjb@gmail.com>
2018-01-22 16:41:05 +1300

    training centre select option displaying with requirements

--- client/components/menuComponents/TrainingCentre.jsx
+++ client/components/menuComponents/TrainingCentre.jsx
@@ -5,8 +5,25 @@ class TrainingCentre extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      levelUpgrade: null
     }
+  }
+  renderLevelOption(requires, level) {
+    const {dungeons} = this.props
+    return dungeons.find(dungeon => dungeon.name == requires && dungeon.isCompleted)
+      ? <option value={2}>Train Recruits to Level 2</option>
+      : <option value={2} disabled>Complete "{requires}" to Unlock Level {level} Training</option>
+  }
+  renderLevelOptions() {
+    const {levelUpgrade} = this.state
+    return <select className="input is-large" name="levelUpgrade" value={levelUpgrade}>
+      <option value={null}>Train to Which Level?</option>
+      {this.renderLevelOption('The Cursed Wilds', 2)}
+      {this.renderLevelOption('The Swamp', 3)}
+      {this.renderLevelOption('The Armory', 4)}
+      {this.renderLevelOption('The Foundry', 5)}
+      {this.renderLevelOption('The Lair', 6)}
+    </select>
   }
   render() {
     const {close} = this.props
@@ -18,6 +35,7 @@ class TrainingCentre extends Component {
           <button onClick={close} className="delete" aria-label="close"></button>
         </header>
         <section className="modal-card-body">
+          {this.renderLevelOptions()}
         </section>
         <footer className="modal-card-foot">
           <button onClick={close} className="button is-large is-info is-outlined is-fullwidth">Leave</button>
@@ -27,9 +45,9 @@ class TrainingCentre extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = ({dungeons}) => {
   return {
-
+    dungeons
   }
 }
 
