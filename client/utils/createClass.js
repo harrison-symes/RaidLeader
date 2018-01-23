acd667037c8be7902c8a77a0115e4e486bfa5fcd
harrison-symes <symeshjb@gmail.com>
2018-01-23 16:01:25 +1300

    class stats rebalanced

--- client/utils/createClass.js
+++ client/utils/createClass.js
@@ -16,17 +16,17 @@ const createMember = (name, level, id, weapon_id, heroClass, hp, power, speed) =
   }
 )
 
-const createPaladin = (name, level, id, weapon_id) => createMember(name, level, id, weapon_id, 'Paladin', (level + 1) * 5, (level * 2), 3)
+const createPaladin = (name, level, id, weapon_id) => createMember(name, level, id, weapon_id, 'Paladin', (level + 1) * 5, ((level + 1) * 2), 2)
 
-const createPriest = (name, level, id, weapon_id) => createMember(name, level, id, weapon_id, 'Priest', (level + 1) * 3, level, 5)
+const createPriest = (name, level, id, weapon_id) => createMember(name, level, id, weapon_id, 'Priest', (level + 1) * 3, level, 4 + level)
 
-const createMonk = (name, level, id, weapon_id) => createMember(name, level, id, weapon_id, 'Monk', (level + 1) * 5, level, 0)
+const createMonk = (name, level, id, weapon_id) => createMember(name, level, id, weapon_id, 'Monk', (level + 1) * 5, level * 2, 0.5)
 
-const createRogue = (name, level, id, weapon_id) => createMember(name, level, id, weapon_id, 'Rogue', (level + 1) * 3, level, 3 + level)
+const createRogue = (name, level, id, weapon_id) => createMember(name, level, id, weapon_id, 'Rogue', (level + 1) * 3, level, 4 + level)
 
-const createMage = (name, level, id, weapon_id) => createMember(name, level, id, weapon_id, 'Mage', (level + 1) * 2, level * 2, 3)
+const createMage = (name, level, id, weapon_id) => createMember(name, level, id, weapon_id, 'Mage', (level + 1) * 2, (level + 1) * 3, 2)
 
-const createWarrior = (name, level, id, weapon_id) => createMember(name, level, id, weapon_id, 'Warrior', (level + 1) * 5, level * 3, 2)
+const createWarrior = (name, level, id, weapon_id) => createMember(name, level, id, weapon_id, 'Warrior', (level + 1) * 5, level * 3, 2.5)
 
 const createWarlock = (name, level, id, weapon_id) => createMember(name, level, id, weapon_id, 'Warlock', (level + 1) * 3, level * 3, 3)
 
