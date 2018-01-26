8aabbc6a64a51a43a4905693b38d36116ca4e35e
harrison-symes <symeshjb@gmail.com>
2018-01-26 17:45:36 +1300

    remove ids from seeds

--- seeds/populateGreetings.js
+++ seeds/populateGreetings.js
@@ -4,10 +4,10 @@ const bcrypt = require('bcrypt')
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('users').del()
-    // .then(function () {
-    //   // Inserts seed entries
-    //   return knex('users').insert([
-    //     {user_id: 1, user_name: 'symeshjb', hash: bcrypt.hashSync('Harrison145', 10), gold: 10000}
-    //   ]);
-    // });
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {user_name: 'symeshjb', hash: bcrypt.hashSync('Harrison145', 10), gold: 10000}
+      ]);
+    });
 };
--- seeds/populateRecruits.js
+++ seeds/populateRecruits.js
@@ -5,14 +5,14 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('recruits').insert([
-        {id: 1, name: 'Patrick', heroClass: 'Paladin', weapon_id: null, level: 2, user_id: 1},
-        {id: 8, name: 'Patty', heroClass: 'Paladin', weapon_id: null, level: 2, user_id: 1},
-        {id: 2, name: 'Penny', heroClass: 'Priest', weapon_id: null, level: 2, user_id: 1},
-        {id: 3, name: 'Mark', heroClass: 'Mage', weapon_id: null, level: 2, user_id: 1},
-        {id: 4, name: 'Wally', heroClass: 'Warlock', weapon_id: null, level: 1, user_id: 1},
-        {id: 5, name: 'Ryan', heroClass: 'Rogue', weapon_id: null, level: 1, user_id: 1},
-        {id: 6, name: 'Weston', heroClass: 'Warrior', weapon_id: null, level: 1, user_id: 1},
-        {id: 7, name: 'Max', heroClass: 'Monk', weapon_id: null, level: 2, user_id: 1},
+        {name: 'Patrick', heroClass: 'Paladin', weapon_id: null, level: 2, user_id: 1},
+        {name: 'Patty', heroClass: 'Paladin', weapon_id: null, level: 2, user_id: 1},
+        {name: 'Penny', heroClass: 'Priest', weapon_id: null, level: 2, user_id: 1},
+        {name: 'Mark', heroClass: 'Mage', weapon_id: null, level: 2, user_id: 1},
+        {name: 'Wally', heroClass: 'Warlock', weapon_id: null, level: 1, user_id: 1},
+        {name: 'Ryan', heroClass: 'Rogue', weapon_id: null, level: 1, user_id: 1},
+        {name: 'Weston', heroClass: 'Warrior', weapon_id: null, level: 1, user_id: 1},
+        {name: 'Max', heroClass: 'Monk', weapon_id: null, level: 2, user_id: 1},
       ]);
     });
 };
