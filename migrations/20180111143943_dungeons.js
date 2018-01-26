e0b1e2a88c2c22c2e5174054e55a63997b5706a1
harrison-symes <symeshjb@gmail.com>
2018-01-26 17:15:49 +1300

    change poor column typing

--- migrations/20180111143943_dungeons.js
+++ migrations/20180111143943_dungeons.js
@@ -4,7 +4,7 @@ exports.up = function(knex, Promise) {
     table.increments('id')
     table.string('name')
     table.string('description')
-    table.integer('requires_complete').defaultTo(null)
+    table.string('requires_complete').defaultTo(null)
     table.integer('max_party')
     table.integer('max_spells')
     table.integer('level').defaultTo(1)
