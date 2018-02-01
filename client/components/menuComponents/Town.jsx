c3a3fae3c2454bf3c3232624461628e981f967b0
harrison-symes <symeshjb@gmail.com>
2018-02-01 19:43:13 +1300

    icons on town screen and tooltips working

--- client/components/menuComponents/Town.jsx
+++ client/components/menuComponents/Town.jsx
@@ -10,7 +10,7 @@ import TrainingCentre from './TrainingCentre'
 import Dungeons from './Dungeons'
 import WeaponStore from './WeaponStore'
 
-const buttonStyle = {height: '10vh', width: '45vw', margin: '1vw'}
+const buttonStyle = {height: '10vh'}
 
 class Town extends Component {
   constructor(props) {
@@ -35,8 +35,16 @@ class Town extends Component {
       default: return null
     }
   }
-  renderTownMenuButton (name) {
-    return <button onClick={() => this.toggleModal(name)} style={buttonStyle} className="column is-6 button is-large is-dark is-outlined is-fullwidth ">{name}</button>
+  renderTownMenuButton (name, icon) {
+    return <a onClick={() => this.toggleModal(name)} style={buttonStyle} className="column is-6 button is-large is-dark is-outlined is-fullwidth ">
+      <span className="icon is-large">
+        <i className={`ra ${icon} ra-2x`}></i>
+      </span>
+      <span className="content is-large"> &nbsp;{name}&nbsp;</span>
+      <span className="icon is-large">
+        <i className={`ra ${icon} ra-2x` }></i>
+      </span>
+    </a>
   }
   render() {
     const {gold, recruits, spellBook} = this.props
@@ -46,26 +54,36 @@ class Town extends Component {
           <p className="title is-1"> <i className="ra  ra-heart-tower ra-fw" /> Town <i className="ra ra-heart-tower ra-fw" /></p>
         <div className="level has-text-centered" style={{width: '50vw', margin: 'auto'}}>
           <div className=" is-4">
-            <p className="subtitle is-3">{spellBook.length}<i className="ra ra-book icon-large" /></p>
+            <div className="tooltip">
+              <span className="tooltiptext">You have learned {spellBook.length} Spells</span>
+              <p className="subtitle is-2">{spellBook.length}<i className="ra ra-book icon-large" /></p>
+            </div>
           </div>
           <div className=" is-4">
-            <p className="subtitle is-3">{gold} <i className="ra ra-gold-bar icon-large" /></p>
+            <div className="tooltip">
+              <span className="tooltiptext">You have {gold} Gold</span>
+              <p className="subtitle is-2">{gold} <i className="ra ra-gold-bar icon-large" /></p>
+            </div>
           </div>
           <div className=" is-4">
-            <p alt={`${recruits.length} recruits`} className="subtitle is-3">{recruits.length} <i className="ra ra-double-team icon-large" /></p>
+            <div className="tooltip">
+              <span className="tooltiptext">You have Recruited {recruits.length} Recruits</span>
+              <p className="subtitle is-2">{recruits.length} <i className="ra ra-double-team icon-large" /></p>
+            </div>
           </div>
         </div>
 
       </div>
       {this.modalSwitch()}
       <div className="hero-body is-fullheight has-text-centered">
-        <button onClick={() => this.toggleModal('Dungeon Map')} style={{height: '15vh', width: '45vw', margin: 'auto', marginBottom: '2vw'}} className="button is-large is-fullwidth is-inverted is-light">Travel</button>
+        <button onClick={() => this.toggleModal('Dungeon Map')} style={{height: '10vh', width: '45vw', margin: 'auto', marginBottom: '2vw'}} className="button is-large is-fullwidth is-inverted is-light">Travel</button>
+
         <div className="columns">
-          {this.renderTownMenuButton('Recruitment Centre')}
-          {this.renderTownMenuButton('Library')}
+          {this.renderTownMenuButton('Recruitment Centre', 'ra-crossed-swords')}
+          {this.renderTownMenuButton('Library', 'ra-crystal-ball')}
         </div>
         <div className="columns">
-          {this.renderTownMenuButton('Training Centre')}
+          {this.renderTownMenuButton('Training Centre', 'ra-muscle-up')}
           <button disabled style={buttonStyle} className="column is-6 button is-large is-dark is-fullwidth is-outlined">Store (WIP)</button>
         </div>
         <div className="columns">
