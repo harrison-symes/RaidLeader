76d71dc0d12363be5a5cc683ba1c6b1ad49218f7
harrison-symes <symeshjb@gmail.com>
2018-02-01 16:45:24 +1300

    remove hero frome home screen

--- client/components/menuComponents/Town.jsx
+++ client/components/menuComponents/Town.jsx
@@ -10,7 +10,7 @@ import TrainingCentre from './TrainingCentre'
 import Dungeons from './Dungeons'
 import WeaponStore from './WeaponStore'
 
-const buttonStyle = {height: '14vh', width: '45vw', margin: '1vw'}
+const buttonStyle = {height: '10vh', width: '45vw', margin: '1vw'}
 
 class Town extends Component {
   constructor(props) {
@@ -36,39 +36,41 @@ class Town extends Component {
     }
   }
   renderTownMenuButton (name) {
-    return <button onClick={() => this.toggleModal(name)} style={buttonStyle} className="column is-6 button is-large is-success is-fullwidth is-outlined is-inverted ">{name}</button>
+    return <button onClick={() => this.toggleModal(name)} style={buttonStyle} className="column is-6 button is-large is-dark is-outlined is-fullwidth ">{name}</button>
   }
   render() {
     const {gold, recruits, spellBook} = this.props
     const {showRecruitmentModal} = this.state
-    return <div style={{height: '89vh', margin: '0'}} className="hero is-bold is-primary">
+    return <div style={{margin: '0'}}>
       <div className="hero-head has-text-centered">
-        <p className="title is-1">Town</p><i className="ra-heavy-shield"></i>
+        <p className="title is-1">Town</p>
         <div className="columns">
           <div className="column is-4">
-            <p className="subtitle is-2">Spells Learnt: {spellBook.length}</p>
+            <p className="subtitle is-4">Spells Learnt: {spellBook.length}</p>
           </div>
           <div className="column is-4">
-            <p className="subtitle is-2">Gold: {gold}</p>
+            <p className="subtitle is-4">Gold: {gold}</p>
           </div>
           <div className="column is-4">
-            <p className="subtitle is-2">Recuits: {recruits.length}</p>
+            <p className="subtitle is-4">Recuits: {recruits.length}</p>
           </div>
         </div>
 
       </div>
       {this.modalSwitch()}
       <div className="hero-body is-fullheight has-text-centered">
-        <button onClick={() => this.toggleModal('Dungeon Map')} style={{height: '15vh', width: '45vw', margin: 'auto', marginBottom: '2vw'}} className="button is-large is-fullwidth is-outlined is-warning">Travel</button>
-        <div className="columns is-multiline">
+        <button onClick={() => this.toggleModal('Dungeon Map')} style={{height: '15vh', width: '45vw', margin: 'auto', marginBottom: '2vw'}} className="button is-large is-fullwidth is-inverted is-light">Travel</button>
+        <div className="columns">
           {this.renderTownMenuButton('Recruitment Centre')}
           {this.renderTownMenuButton('Library')}
-          {/* <button disabled style={buttonStyle} className="column is-6 button is-large is-success is-fullwidth is-outlined is-inverted ">Training (WIP)</button> */}
+        </div>
+        <div className="columns">
           {this.renderTownMenuButton('Training Centre')}
-          <button disabled style={buttonStyle} className="column is-6 button is-large is-success is-fullwidth is-outlined is-inverted ">Store (WIP)</button>
-          {/* {this.renderTownMenuButton('Weapon Store')} */}
-          <Link to="/" style={buttonStyle} className="button is-large is-fullwidth is-danger is-outlined is-inverted" onClick={() => this.props.dispatch(logoutUser())}>Logout</Link>
-          <div style={buttonStyle} disabled className="button is-large is-fullwidth is-primary is-outlined is-inverted">Profile (WIP)</div>
+          <button disabled style={buttonStyle} className="column is-6 button is-large is-dark is-fullwidth is-outlined">Store (WIP)</button>
+        </div>
+        <div className="columns">
+          <Link to="/" style={buttonStyle} className="button is-large is-fullwidth is-danger is-outlined" onClick={() => this.props.dispatch(logoutUser())}>Logout</Link>
+          <div style={buttonStyle} disabled className="button is-large is-fullwidth is-dark is-outlined">Profile (WIP)</div>
         </div>
       </div>
     </div>
