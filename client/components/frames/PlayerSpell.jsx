2a0290d6a3765a03ac09751a49871fe8e6006f75
harrison-symes <symeshjb@gmail.com>
2018-02-06 17:02:52 +1300

    back to buttons

--- client/components/frames/PlayerSpell.jsx
+++ client/components/frames/PlayerSpell.jsx
@@ -116,7 +116,7 @@ class PlayerSpell extends Component {
     let width = 1000 / player.spells.length
     if (width > 200) width = 200
     return <div
-      className={`PlayerSpell`}
+      className={`PlayerSpell button`}
       onClick={() => this.clickSpell()}
       style={{width: `${width}px`}}>
       <table className="box">
