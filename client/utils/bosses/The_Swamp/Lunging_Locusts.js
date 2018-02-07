d4c7fff4d4d5f4a12fe04977092d37d266bc562b
harrison-symes <symeshjb@gmail.com>
2018-02-07 17:05:20 +1300

    fix issue with plagued sword across all swamp bosses

--- client/utils/bosses/The_Swamp/Lunging_Locusts.js
+++ client/utils/bosses/The_Swamp/Lunging_Locusts.js
@@ -14,7 +14,7 @@ export default {
   isCasting: false,
   bossTarget: null,
   description: "As you explore deeper into the Swamp, you see less life around you, and a soft buzzing sound starts getting louder... and louder...",
-  weaponRewards: ['Plagued Aegis', 'Plagued Daggers', 'Plagued Wand', 'Plagued Sword', 'Plagued Stave', 'Plagued Scythe', 'Glowing Libram', 'Plagued Staff of the Swamp'],
+  weaponRewards: ['Plagued Aegis', 'Plagued Daggers', 'Plagued Wand', 'Plagued Blade', 'Plagued Stave', 'Plagued Scythe', 'Glowing Libram', 'Plagued Staff of the Swamp'],
   goldReward: 500,
   weaponChance: 1,
   progress_required: 1,
