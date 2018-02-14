9f48695090899f1f58fad76d071263adad0a691a
harrison-symes <symeshjb@gmail.com>
2018-02-14 13:56:58 +1300

    change progress bar for recruits

--- client/components/frames/RecruitHealthBar.jsx
+++ client/components/frames/RecruitHealthBar.jsx
@@ -3,21 +3,25 @@ import {connect} from 'react-redux'
 import {HealthIcon, SpeedIcon, PowerIcon} from '../icons/StatIcons'
 import EffectTag from './EffectTag'
 
+import { Line, Circle } from 'rc-progress';
+
+
 const RecruitHealthBar = ({recruit}) => {
   let {hp, initHp, power, speed, effects} = recruit
     const percent = hp/initHp * 100
   const colourClass = percent > 1/3*100
     ? percent > 2/3*100
       ? percent == 100
-        ? 'is-primary'
-        : 'is-success'
-      : 'is-warning'
-    : 'is-danger'
+        ? 'blue'
+        : 'lightgreen'
+      : 'yellow'
+    : 'red'
   return <div className="RecruitHealthBar has-text-centered">
     <div className="">
       <div className="columns">
         <div className="column is-9">
-          <progress className={`ProgressBar progress ${colourClass}`} max="100" value={percent}>{percent}%</progress>
+          {/* <progress className={`ProgressBar progress ${colourClass}`} max="100" value={percent}>{percent}%</progress> */}
+          <Line percent={percent} strokeWidth="4" strokeColor={colourClass} strokeLineCap="square" />
         </div>
         <div className="column is-3"><p className="subtitle is-4"><HealthIcon value={`${Math.round(hp * 10) /10} / ${Math.round(initHp * 10) /10}`} /></p></div>
       </div>
