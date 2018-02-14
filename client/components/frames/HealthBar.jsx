289e52f14155ec252efb678023c853076fc389be
harrison-symes <symeshjb@gmail.com>
2018-02-14 14:15:12 +1300

    rework and change progress bars for player frame

--- client/components/frames/HealthBar.jsx
+++ client/components/frames/HealthBar.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {HealthIcon} from '../icons/StatIcons'
 
+import { Line, Circle } from 'rc-progress';
+
+
 const HealthBar = ({maxHP, hp}) => {
   const percent = hp/maxHP * 100
   const colourClass = percent > 1/3*100
     ? percent > 2/3*100
       ? percent == 100
-        ? 'is-primary'
-        : 'is-success'
-      : 'is-warning'
-    : 'is-danger'
+        ? 'blue'
+        : 'lightgreen'
+      : 'yellow'
+    : 'red'
   return <div className="HealthBar has-text-centered">
-    <p className="subtitle is-4"><HealthIcon value={`${hp} / ${maxHP}`} /></p>
-    <div className="box">
-      <progress className={`ProgressBar progress ${colourClass}`} max="100" value={percent}>{percent}%</progress>
+    <p className="content"><HealthIcon value={`${hp} / ${maxHP}`} /></p>
+    <div className="box" style={{height: '15px'}}>
+      <Line percent={percent} strokeWidth="10" strokeColor={colourClass} strokeLineCap="square" />
     </div>
   </div>
 }
