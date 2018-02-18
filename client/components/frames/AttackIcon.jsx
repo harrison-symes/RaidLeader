3e603cd2b7cff4030a7fed525ecdcdb1c177b94b
harrison-symes <symeshjb@gmail.com>
2018-02-19 10:25:52 +1300

    attack icon moves towards boss icon

--- client/components/frames/AttackIcon.jsx
+++ client/components/frames/AttackIcon.jsx
@@ -3,12 +3,15 @@ import React, {Component} from 'react'
 class AttackIcon extends Component {
   constructor(props) {
     super(props)
+    const {startX, startY} = props.svg
     var bossFrame = document.getElementById('BossIcon').getBoundingClientRect()
     this.state = {
-      x: props.svg.startX || 10,
-      y: props.svg.startY || 10,
+      x: startX || 10,
+      y: startY || 10,
       targetX: bossFrame.left,
-      targetY: bossFrame.top
+      targetY: bossFrame.top,
+      speedY: (startY - bossFrame.top) / 30,
+      speedX: (startX - bossFrame.left) / 30
     }
   }
   componentDidMount() {
@@ -18,12 +21,13 @@ class AttackIcon extends Component {
     setTimeout(() => this.endTick(), 10)
   }
   endTick() {
-    let {y, targetY} = this.state
-    y--
-    if (y <= targetY) this.props.deleteSVG(this.props.svg)
+    let {y, x, targetY, targetX, speedY, speedX} = this.state
+    if (y > targetY) y-=speedY
+    if (x > targetX) x-=speedX
+    if (y <= targetY && x <= targetX) this.props.deleteSVG(this.props.svg)
     else {
-      this.setState({y})
       this.startTick()
+      this.setState({y, x})
     }
   }
   render() {
