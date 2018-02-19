db1d7da85defeda8a22477496272c9a291e1872e
harrison-symes <symeshjb@gmail.com>
2018-02-19 14:46:31 +1300

    boss health bar boxes corrected

--- client/components/frames/BossHealthBar.jsx
+++ client/components/frames/BossHealthBar.jsx
@@ -52,7 +52,7 @@ const BossHealthBar = ({boss}) => {
         : 'is-success'
       : 'is-warning'
     : 'is-danger'
-    return <div className="BossHealthBar columns has-text-centered" style={{backgroundColor: 'white', maxHeight: '85px'}}>
+    return <div className="BossHealthBar columns has-text-centered" style={{backgroundColor: 'white', maxHeight: '85px', backgroundImage: 'inherit', margin: 0, padding: 0, left: 0}}>
       {healthBar(hp, initHp, armor)}
       {armor > 0 && armorBar(armor, initArmor)}
       {manaBar(mana, maxMana)}
