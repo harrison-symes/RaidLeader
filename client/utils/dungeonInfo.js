5a638287133c7532359b359e627a30c3c23363b4
harrison-symes <symeshjb@gmail.com>
2018-02-20 14:29:23 +1300

    convert dungeon text to from db to client side

--- client/utils/dungeonInfo.js
+++ client/utils/dungeonInfo.js
@@ -30,3 +30,91 @@ export function MenuBackground (dungeon) {
     }
   }
 }
+
+export default function getDungeonInfo(dungeon, is_complete) {
+  switch(dungeon) {
+    case 'The Hunt': return {
+      name: 'The Hunt',
+      level: 1,
+      is_repeatable: false,
+      max_party: 1,
+      max_spells: 1,
+      requires_complete: null,
+      rewards: [],
+      gold_reward: 500,
+      description: "The Paladin knows the location of a dying Dragon. Dragons are pretty scary, but they always hoard a large amount of treasure. Hopefully this Dragon won't live long enough to burn you alive...",
+      bosses: [
+        'Damaged Dragon'
+      ]
+    }
+    case 'The Cursed Wilds': return {
+      name: 'The Cursed Wilds',
+      level: 1,
+      is_repeatable: true,
+      max_party: 2,
+      max_spells: 2,
+      requires_complete: 'The Hunt', rewards: [
+        {
+          name: 'Cursed Staff of the Wilds',
+          min: 0,
+          max: 0.7
+        },
+        {
+          name: 'Cleansed Staff of the Wilds',
+          min: 0.7,
+          max: 1
+        }
+      ],
+      gold_reward: 500,
+      description: "Patrons of the local Town Pub have told stories of the Cursed Wilds, filled with giant beasts who have been acting more aggressively lately. Will you brave the challenge and free these Beasts from the curse?",
+      bosses: [
+        "Trampling Turtle",
+        "Biting Bear",
+        "Spitting Spider"
+      ]
+    }
+    case 'The Swamp': return {
+      name: 'The Swamp',
+      level: 2,
+      is_repeatable: true,
+      max_party: 3,
+      max_spells: 3,
+      requires_complete: 'The Cursed Wilds',
+      rewards: [
+        {
+          name: 'Plagued Staff of the Swamp',
+          min: 0,
+          max: 0.7
+        }, {
+          name: 'Cleansed Staff of the Woods',
+          min: 0.7,
+          max: 1
+        }
+      ],
+      gold_reward: 1000,
+      description: "The once tranquil woods have been overwhelmed by a strange sludge in recent weeks. Many adventurers have sought the source of this plague, but none have returned. That probably means there is a lot of loot to be found here!",
+      bosses: [
+        "Seeping Slime",
+        "Decaying Deer",
+        "Lunging Locusts",
+        "Plague Piltherer"
+      ]
+    }
+    case "The Foundry": return {
+      name: 'The Foundry',
+      level: 3,
+      is_repeatable: true,
+      max_party: 3,
+      max_spells: 3,
+      requires_complete: 'The Swamp',
+      rewards: [
+
+      ],
+      gold_reward: 1500,
+      description: "A great Foundry resides within an active Volcano. You're not sure who, what or why, but SOMEONE is definitely doing SOMETHING here for SOME REASON...",
+      bosses: [
+        "Flaming Furnace"
+      ]
+    }
+  }
+}
