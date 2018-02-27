9899a81e1b744c7c630d41a80795008061a949ef
harrison-symes <symeshjb@gmail.com>
2018-02-27 15:14:16 +1300

    Started reducer tests

---
+++ test/client/reducers/started.test.js
@@ -0,0 +1,45 @@
+import reducer from '../../../client/reducers/started'
+
+test('started initial state', () => {
+  expect(reducer(undefined, {})).toBe(false)
+})
+
+test('LOGOUT', () => {
+  const actual = reducer(true,
+  {
+    type: 'LOGOUT'
+  })
+  expect(actual).toBe(false)
+})
+
+test('TRAVEL_TO_TOWN', () => {
+  const actual = reducer(true,
+  {
+    type: 'TRAVEL_TO_TOWN'
+  })
+  expect(actual).toBe(false)
+})
+
+test('START', () => {
+  const actual = reducer(false,
+  {
+    type: 'START'
+  })
+  expect(actual).toBe(true)
+})
+
+test('GAME_WON', () => {
+  const actual = reducer(true,
+  {
+    type: 'GAME_WON'
+  })
+  expect(actual).toBe(false)
+})
+
+test('GAME_LOST', () => {
+  const actual = reducer(true,
+  {
+    type: 'GAME_LOST'
+  })
+  expect(actual).toBe(false)
+})
