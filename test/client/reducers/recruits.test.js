47d087aafb2d5613508acb117c335d75bd5ba6ad
harrison-symes <symeshjb@gmail.com>
2018-02-27 16:59:26 +1300

    recruits reducer tested

---
+++ test/client/reducers/recruits.test.js
@@ -0,0 +1,73 @@
+import reducer from '../../../client/reducers/recruits'
+
+test('initial state recruits', () => {
+  expect(reducer(undefined, {})).toEqual([])
+})
+
+test('LOGOUT', () => {
+  const actual = reducer([{}, {}], {
+    type: 'LOGOUT'
+  })
+  expect(actual).toEqual([])
+})
+
+test('RECEIVE_RECRUITS', () => {
+  const actual = reducer([], {
+    type: 'RECEIVE_RECRUITS',
+    recruits: [{}, {}]
+  })
+  expect(actual).toEqual([{}, {}])
+})
+
+test('ADD_RECRUIT', () => {
+  const actual = reducer([{id: 1}], {
+    type: 'ADD_RECRUIT',
+    recruit: {id: 2}
+  })
+  expect(actual).toEqual([
+    {id:1}, {id: 2}
+  ])
+})
+
+test('RECRUIT_EQUIP_WEAPON', () => {
+  const initial = [
+    {id: 1, name: 'Jeff', weapon_id: null},
+    {id: 2, name: 'Not-Jeff', weapon_id: 1}
+  ]
+  const actual = reducer(initial, {
+    type: 'RECRUIT_EQUIP_WEAPON',
+    recruit: {id: 1, name: 'Jeff', weapon_id: null},
+    weapon_id: 3
+  })
+  expect(actual).toEqual([
+    {id: 1, name: 'Jeff', weapon_id: 3},
+    {id: 2, name: 'Not-Jeff', weapon_id: 1},
+  ])
+})
+
+test('UPDATE_RECRUIT (no recruit)', () => {
+  const initial = [
+    {id: 1, name: 'Jeff', weapon_id: null},
+    {id: 2, name: 'Not-Jeff', weapon_id: 1}
+  ]
+  const actual = reducer(initial, {
+    type: 'UPDATE_RECRUIT',
+    recruit: null
+  })
+  expect(actual).toEqual(initial)
+})
+
+test('UPDATE_RECRUIT (with data)', () => {
+  const initial = [
+    {id: 1, level: 1},
+    {id: 2, level: 1}
+  ]
+  const actual = reducer(initial, {
+    type: 'UPDATE_RECRUIT',
+    recruit: {id: 1, level: 2}
+  })
+  expect(actual).toEqual([
+    {id: 1, level: 2},
+    {id: 2, level: 1}
+  ])
+})
