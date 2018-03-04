fe8935c277766ed5c0526009ea8c4b93fa5a905e
harrison-symes <symeshjb@gmail.com>
2018-03-05 12:05:25 +1300

    full coverage on connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,5 @@
 var Knex = require('knex')
 var config = require('../../knexfile')[process.env.NODE_ENV || 'development']
-// console.log({config});
 var knex = Knex(config)
 
 module.exports = knex
--- test/server/db/connection.test.js
+++ test/server/db/connection.test.js
@@ -1,7 +1,9 @@
 test('test connection with no NODE_ENV', () => {
+  delete process.env.NODE_ENV
+  console.log(process.env.NODE_ENV);
   const actual = require('../../../server/db/connection').client.config
 
-  var expected = require('../../../knexfile').test
+  var expected = require('../../../knexfile').development
   // console.log({actual, expected});
   // console.log(actual.client.config);
   expect(actual).toEqual(expected)
