2db41cd76669a2c6a2f9b474fd988f252b191e37
harrison-symes <symeshjb@gmail.com>
2018-03-05 12:12:47 +1300

    auth hash coverage testing

--- test/server/db/connection.test.js
+++ test/server/db/connection.test.js
@@ -1,6 +1,5 @@
 test('test connection with no NODE_ENV', () => {
   delete process.env.NODE_ENV
-  console.log(process.env.NODE_ENV);
   const actual = require('../../../server/db/connection').client.config
 
   var expected = require('../../../knexfile').development
