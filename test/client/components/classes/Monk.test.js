1fd95fdfe2b74e0172613ccaabfec44314474167
harrison-symes <symeshjb@gmail.com>
2018-03-05 17:38:08 +1300

    Monk component coverage tests

--- test/client/components/classes/Monk.test.js
+++ test/client/components/classes/Monk.test.js
@@ -15,19 +15,13 @@ jest.mock('../../../../client/utils/localstorage', () => ({
 const member = {
   id: 1,
   isAlive: true,
-  name: 'Jeff',
   power: 10,
-  speed: 5,
-  initHp: 100,
-  hp: 60,
   weapon_effect: null
 }
 
 const boss = {
   bossTarget: member
 }
-
-const dispatch = jest.fn()
 
 test('Monk attack basic success', () => {
   const store = mockStore()
@@ -36,9 +30,7 @@ test('Monk attack basic success', () => {
   }
 
   const wrapper = shallow(<Monk
-    store={store}
     member={clone(member)}
-    boss={clone(boss)}
     started={true}
     dispatch={store.dispatch}
   />)
@@ -55,5 +47,89 @@ test('Monk attack basic success', () => {
   expect(actions[1]).toEqual({
     type: 'PERCENT_HEAL_ALL_FRIENDLY',
     percentage: 0.1
+  })
+})
+
+test('Monk attack while dead', () => {
+  const store = mockStore()
+  Monk.prototype.render = function () {
+    return <div></div>
+  }
+
+  const fakeMember = {
+    ...member,
+    isAlive: false
+  }
+
+  const wrapper = shallow(<Monk
+    member={fakeMember}
+    started={true}
+    dispatch={store.dispatch}
+  />)
+  wrapper.instance().finishCast()
+
+  const actions = store.getActions()
+
+  expect(actions).toHaveLength(0)
+})
+
+test('Monk attack taunt basic success', () => {
+  const store = mockStore()
+  Monk.prototype.render = function () {
+    return <div></div>
+  }
+
+  const fakeMember = {
+    ...member,
+    weapon_effect: 'taunt'
+  }
+
+  const wrapper = shallow(<Monk
+    member={fakeMember}
+    started={true}
+    dispatch={store.dispatch}
+  />)
+  wrapper.instance().finishCast()
+
+  const actions = store.getActions()
+
+  expect(actions).toHaveLength(3)
+
+  expect(actions[0]).toEqual({
+    type: 'BOSS_CHANGE_TARGET',
+    target: fakeMember
+  })
+  expect(actions[1]).toEqual({
+    type: 'PHYSICAL_ATTACK_BOSS',
+    power: 10
+  })
+  expect(actions[2]).toEqual({
+    type: 'PERCENT_HEAL_ALL_FRIENDLY',
+    percentage: 0.1
+  })
+})
+
+test('Monk startFighting', () => {
+  const store = mockStore()
+  Monk.prototype.render = function () {
+    return <div></div>
+  }
+
+  const wrapper = shallow(<Monk
+    member={clone(member)}
+    started={true}
+    dispatch={store.dispatch}
+  />)
+  wrapper.instance().startFighting()
+
+  const actions = store.getActions()
+
+
+
+  expect(actions).toHaveLength(1)
+
+  expect(actions[0]).toEqual({
+    type: 'MONK_START_BUFF',
+    target: member
   })
 })
