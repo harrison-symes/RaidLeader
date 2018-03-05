70e30103453ef5fe96d6736009afc2c40c6ba7ab
harrison-symes <symeshjb@gmail.com>
2018-03-06 08:40:55 +1300

    shaman component coverage tests

--- test/client/components/classes/Shaman.test.js
+++ test/client/components/classes/Shaman.test.js
@@ -64,6 +64,92 @@ test('Shaman attack basic success', () => {
   })
 
 })
+
+test('Shaman attack renew logic test', () => {
+  const store = mockStore()
+  Shaman.prototype.render = () => <div></div>
+
+  const fakeParty = clone(party)
+  const fakeMember = {
+    id: 1,
+    initHp: 100,
+    hp: 1,
+    isAlive: true,
+    power: 10,
+    weapon_effect: null
+  }
+  fakeParty[0] = fakeMember
+
+  const wrapper = shallow(<Shaman
+    member={fakeMember}
+    party={fakeParty}
+    started={true}
+    dispatch={store.dispatch}
+  />)
+  wrapper.instance().finishCast()
+
+  const actions = store.getActions()
+
+  expect(actions).toHaveLength(2)
+
+  expect(actions[0]).toEqual({
+    type: 'ADD_EFFECT_TO_TARGET',
+    effect: {name: 'Renew'},
+    target: fakeMember
+  })
+
+  expect(actions[1]).toEqual({
+    type: 'PHYSICAL_ATTACK_BOSS',
+    power: 10
+  })
+
+})
+
+test('Shaman curePoison test', () => {
+  const store = mockStore()
+  Shaman.prototype.render = () => <div></div>
+
+  const fakeParty = clone(party)
+  const fakeMember = {
+    id: 1,
+    initHp: 100,
+    hp: 1,
+    isAlive: true,
+    power: 10,
+    weapon_effect: 'curePoison'
+  }
+  fakeParty[0] = fakeMember
+
+  const wrapper = shallow(<Shaman
+    member={fakeMember}
+    party={fakeParty}
+    started={true}
+    dispatch={store.dispatch}
+  />)
+  wrapper.instance().finishCast()
+
+  const actions = store.getActions()
+
+  expect(actions).toHaveLength(3)
+  expect(actions[0]).toEqual({
+    type: 'REMOVE_EFFECT_FROM_TARGET',
+    effect: {name: 'Poison'},
+    target: fakeMember
+  })
+
+  expect(actions[1]).toEqual({
+    type: 'ADD_EFFECT_TO_TARGET',
+    effect: {name: 'Renew'},
+    target: fakeMember
+  })
+
+
+  expect(actions[2]).toEqual({
+    type: 'PHYSICAL_ATTACK_BOSS',
+    power: 10
+  })
+
+})
 //
 // test('Shaman attack taunt weapon', () => {
 //   const store = mockStore()
@@ -124,49 +210,49 @@ test('Shaman attack basic success', () => {
 //     power: 10
 //   })
 // })
-//
-// test('Shaman attack not alive', () => {
-//   const store = mockStore()
-//   Shaman.prototype.render = () => <div></div>
-//
-//   const fakeMember = {
-//     ...member,
-//     isAlive: false
-//   }
-//
-//   const wrapper = shallow(<Shaman
-//     member={fakeMember}
-//     boss={boss}
-//     started={true}
-//     dispatch={store.dispatch}
-//   />)
-//
-//   wrapper.instance().finishCast()
-//
-//   const actions = store.getActions()
-//
-//   expect(actions).toHaveLength(0)
-// })
-//
-// test('Shaman startFighting', () => {
-//   const store = mockStore()
-//   Shaman.prototype.render = () =>  <div></div>
-//   Shaman.prototype.startCast = () => ({})
-//
-//   const wrapper = shallow(<Shaman
-//     member={member}
-//     started={true}
-//     dispatch={store.dispatch}
-//   />)
-//
-//   wrapper.instance().startFighting()
-//
-//   const actions = store.getActions()
-//
-//   expect(actions).toHaveLength(1)
-//
-//   expect(actions[0]).toEqual({
-//     type: 'WARRIOR_START_BUFF',
-//     target: member
-//   })
-// })
+
+test('Shaman attack not alive', () => {
+  const store = mockStore()
+  Shaman.prototype.render = () => <div></div>
+
+  const fakeMember = {
+    ...member,
+    isAlive: false
+  }
+
+  const wrapper = shallow(<Shaman
+    member={fakeMember}
+    party={party}
+    started={true}
+    dispatch={store.dispatch}
+  />)
+
+  wrapper.instance().finishCast()
+
+  const actions = store.getActions()
+
+  expect(actions).toHaveLength(0)
+})
+
+test('Shaman startFighting', () => {
+  const store = mockStore()
+  Shaman.prototype.render = () =>  <div></div>
+  Shaman.prototype.startCast = () => ({})
+
+  const wrapper = shallow(<Shaman
+    member={member}
+    party={party}
+    started={true}
+    dispatch={store.dispatch}
+  />)
+
+  wrapper.instance().startFighting()
+
+  const actions = store.getActions()
+
+  expect(actions).toHaveLength(1)
+
+  expect(actions[0]).toEqual({
+    type: 'SHAMAN_START_BUFF'
+  })
+})
