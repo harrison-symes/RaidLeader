e6e39da09b9ac2eb117ea41db37049be7634bcfb
harrison-symes <symeshjb@gmail.com>
2018-03-20 10:33:33 +1300

    My Recruits displayed by class

--- client/components/menuComponents/MyRecruits.jsx
+++ client/components/menuComponents/MyRecruits.jsx
@@ -3,6 +3,21 @@ import {connect} from 'react-redux'
 
 import RecruitFrame from './RecruitFrame'
 
+import {ClassIcon} from '../icons/StatIcons'
+
+const classes = [
+  'Paladin',
+  'Warrior',
+  'Rogue',
+  'Mage',
+  'Hunter',
+  'Warlock',
+  'Priest',
+  'Monk',
+  'Shaman',
+  'Bard'
+]
+
 class MyRecruits extends Component {
   constructor(props) {
     super(props)
@@ -18,22 +33,35 @@ class MyRecruits extends Component {
   selectRecruit(selected) {
     this.setState({selected})
   }
-  renderContent() {
-    const {recruits} = this.props
-
-    return <div className="columns is-multiline">
-      {recruits.map(recruit =>
-        <table className="column is-4 table has-text-centered is-full-width">
-          <RecruitFrame
-            selectRecruit={this.selectRecruit}
-            addRecruit={null}
-            inParty={false}
-            key={`recruit-${recruit.id}`}
-            recruit={recruit}
-          />
-        </table>
-      )}
+  recruitsByClass(heroClass) {
+    const recruits = this.props.recruits.filter(recruit => recruit.heroClass == heroClass)
+    if (recruits.length == 0) return null
+    return <div className="has-text-centered">
+      <h1 className="title is-3">
+        <ClassIcon heroClass={heroClass} />
+        &nbsp;{heroClass}s ({recruits.length})&nbsp;
+        <ClassIcon heroClass={heroClass} />
+      </h1>
+      <br />
+      <div className="columns is-multiline">
+        {recruits.map(recruit =>
+          <table className="column is-4 table has-text-centered is-full-width">
+            <RecruitFrame
+              selectRecruit={this.selectRecruit}
+              addRecruit={null}
+              inParty={false}
+              key={`recruit-${recruit.id}`}
+              recruit={recruit}
+            />
+          </table>
+        )}
+      </div>
+      <br />
+      <br />
     </div>
+  }
+  renderContent() {
+    return classes.map(heroClass => this.recruitsByClass(heroClass))
   }
   render() {
     const {selected} = this.state
