546a0604f633605b60e051b042ad7423246461de
harrison-symes <symeshjb@gmail.com>
2018-03-21 09:28:58 +1300

    Rework of Home Page

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -2,29 +2,41 @@ import React from 'react'
 import Login from './Login'
 import Register from './Register'
 
+import {ClassIcon} from './icons/StatIcons'
+
+const classes = [
+  'Paladin',
+  'Warrior',
+  'Rogue',
+  'Mage',
+  'Hunter',
+  'Warlock',
+  'Priest',
+  'Monk',
+  'Shaman',
+  'Bard'
+]
+
 import {HashRouter as Router, Route, Link} from 'react-router-dom'
 
 export default function Home (props) {
-  return <div className="">
-    <div className="hero is-fullheight is-bold is-light has-text-centered">
-      <div className="hero-head has-text-centered">
+  return <Router>
+    <div className="Town">
+      <div className="Town-Banner has-text-centered">
+        <Link to="/" className="title is-1">Raid Leader v0.3</Link>
       </div>
-      <div className="hero-body has-text-centered">
-        <Router>
-          <div>
-            <div className="container" style={{width: '50vw', marginLeft: '25vw'}}>
-              <h1 className="title is-1">Welcome to Raid Leader v0.3</h1>
-              <Route exact path='/' component={() => <div className=" has-text-centered" >
-                <Link to="/login" className="button is-large is-fullwidth is-info">Login</Link>
-                <Link to="/register" className="button is-large is-fullwidth is-primary">Register</Link>
-              </div>} />
-              <Route path="/login" component={Login} />
-              <Route path="/Register" component={Register} />
-            </div>
-          </div>
-        </Router>
+      <div className="Town-Buttons" >
+        <div className="subtitle is-1 has-text-centered">
+          {classes.map(heroClass => <ClassIcon heroClass={heroClass} />)}
+        </div>
+        <Route exact path='/' component={() => <div style={{marginTop: '10%'}}>
+          <Link to="/login" className="column is-6 is-offset-3 button is-large is-fullwidth is-info is-outlined">Login</Link>
+          <Link to="/register" className="column is-6 is-offset-3 button is-large is-fullwidth is-primary is-outlined">Register</Link>
+          <Link to="/about" className="column is-6 is-offset-3 button is-large is-fullwidth is-warning is-outlined" disabled>About (WIP)</Link>
+        </div>} />
+        <Route path="/login" component={Login} />
+        <Route path="/Register" component={Register} />
       </div>
     </div>
-
-  </div>
+  </Router>
 }
