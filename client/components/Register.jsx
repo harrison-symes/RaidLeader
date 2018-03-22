40ad025bb5dd977e6eaad5126e96c8813e1c5403
harrison-symes <symeshjb@gmail.com>
2018-03-22 14:43:17 +1300

    Register Icons"

--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -19,6 +19,18 @@ class Register extends React.Component {
   updateDetails(e) {
     this.setState({[e.target.name]: e.target.value})
   }
+  validatePassword() {
+    const {password} = this.state
+    return password.length >= 6 && password.length <= 15
+  }
+  validateUsername() {
+    const {user_name} = this.state
+    return user_name.length >= 6 && user_name.length <= 15
+  }
+  validateConfirmPassword() {
+    const {confirm_password, password} = this.state
+    return password == confirm_password && this.validatePassword()
+  }
   submit(e) {
     e.preventDefault()
     e.target.reset()
@@ -33,31 +45,49 @@ class Register extends React.Component {
     }))
     else this.setState({errorMessage: 'Passwords Do Not Match'})
   }
+  inputIcon (icon, isLeft, isDone) {
+    return <span className={`icon is-${isLeft ? 'left': 'right'}`}>
+      <i className={`ra ra-fw ${icon}`} style={{color: isDone ? 'lightgreen' : 'grey '}}/>
+    </span>
+  }
   render() {
     return (
       <form className="box column is-6 is-offset-3 form has-text-centered" style={{ background: 'inherit', backgroundImage: '', borderRadius: '15%'}} onSubmit={this.submit}>
         <p className="title is-1">Register</p>
         {this.state.errorMessage && <h1 className="box has-text-danger">{this.state.errorMessage}</h1>}
         <label className="column is-8 is-offset-2 label is-large has-text-light">Username:
-          <input
-            required
-            className=" input is-large is-primary"
-            type="text"
-            name="user_name"
-            onChange={this.updateDetails}
-          />
+          <div className="control has-icons-left has-icons-right">
+            <input
+              required
+              className=" input is-large is-primary"
+              type="text"
+              name="user_name"
+              onChange={this.updateDetails}
+            />
+            {this.inputIcon('ra-player-king', true, this.validateUsername())}
+            {this.inputIcon('ra-player-king', false, this.validateUsername())}
+          </div>
         </label><br/>
         <div className="columns">
           <label className={`label column is-large has-text-light`}>Password:
-            <input className=" input is-large is-primary"
-              required
-              type="password" name="password" onChange={this.updateDetails}
-            />
+            <div className="control has-icons-left has-icons-right">
+              <input className=" input is-large is-primary"
+                required
+                type="password" name="password" onChange={this.updateDetails}
+              />
+              {this.inputIcon('ra-uncertainty', true, this.validatePassword())}
+              {this.inputIcon('ra-uncertainty', false, this.validatePassword())}
+            </div>
           </label>
           <label className={`label column is-offset-1 is-large has-text-light`}>Confirm:
-            <input className=" input is-large is-primary"
-              required
-              type="password" name="confirm_password" onChange={this.updateDetails}/>
+            <div className="control has-icons-left has-icons-right">
+              <input className=" input is-large is-primary"
+                required
+                type="password" name="confirm_password" onChange={this.updateDetails}
+              />
+              {this.inputIcon('ra-interdiction', true, this.validateConfirmPassword())}
+              {this.inputIcon('ra-interdiction', false, this.validateConfirmPassword())}
+            </div>
           </label>
         </div>
         <input className="button column is-6 is-offset-3 is-large is-success is-fullwidth is-outlined has-text-light" type="submit" />
