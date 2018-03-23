5dd776b9627d2b4f12c050980cb42f6f01b9f2cd
harrison-symes <symeshjb@gmail.com>
2018-03-23 19:23:11 +1300

    Fix Warlock Weapon from foundry

--- client/components/classes/Warlock.jsx
+++ client/components/classes/Warlock.jsx
@@ -10,7 +10,7 @@ export class Warlock extends PartyMemberFrame {
     let {power, isAlive, weapon_effect} = member
     if (isAlive && started) {
       dispatch({type: 'PHYSICAL_ATTACK_BOSS', power})
-      if (weapon_effect == 'lightningRod') dispatch({type: 'PERCENT_DAMAGE_DAMAGE_FRIENDLY_TARGET', percentage: 0.05})
+      if (weapon_effect == 'lightningRod') dispatch({type: 'PERCENT_DAMAGE_DAMAGE_FRIENDLY_TARGET', target: member, percentage: 0.05})
       else dispatch({type: 'PERCENT_DAMAGE_DAMAGE_ALL_FRIENDLY', percentage: 0.05, target: member})
     }
   }
