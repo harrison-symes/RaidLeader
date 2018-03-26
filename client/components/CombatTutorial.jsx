4f37f105c97fe1ef069136e707ebffffffe675e5
harrison-symes <symeshjb@gmail.com>
2018-03-26 14:48:20 +1300

    Tooltip driven first 3 tutorial steps

--- client/components/CombatTutorial.jsx
+++ client/components/CombatTutorial.jsx
@@ -6,6 +6,7 @@ import HealthBar from './frames/HealthBar'
 import ManaBar from './frames/ManaBar'
 
 import { Line, Circle } from 'rc-progress';
+import {Tooltip} from 'react-tippy'
 
 export default class CombatTutorial extends Component {
   constructor(props) {
@@ -22,26 +23,34 @@ export default class CombatTutorial extends Component {
       stepCompleted: false
     }
   }
-  tick() {
-    if (this.state.stepCompleted) return <i className="icon fas fa-check" style={{color: 'lightgreen'}} />
-    else return null
+  componentWillUpdate(props, state) {
+    if (state.stepCompleted == true) setTimeout(() => this.setState({stepCompleted: false, step: state.step + 1}), 1000)
   }
-  completeStep(step) {
+  toggleCompletion() {
+    this.setState({stepCompleted: !this.state.stepCompleted})
+  }
+  completeStep() {
     this.setState({stepCompleted: true})
-    setTimeout(() => this.setState({step: step+1, stepCompleted: false}), 1000)
   }
   targetPaladin() {
     if (this.state.step == 1) this.completeStep(1)
+    else if (this.state.step == 3) this.completeStep(3)
     this.setState({target: this.props.paladinName})
   }
   targetPlayer() {
+    if (this.state.step == 2) this.completeStep(2)
     this.setState({target: 'Player'})
   }
   messageSwitch() {
-
     const {step, stepCompleted} = this.state
+    console.log({stepCompleted});
     switch(step) {
-      case 1: return <p className="message-content">Click on <b>{this.props.paladinName}</b> to <b>Target</b> them.{this.tick()}</p>
+      case 1: return <p style={{color: stepCompleted ? 'lightgreen' : 'orange'}} className="message-content">Click on <b>{this.props.paladinName}</b> to <b>Target</b> them.  </p>
+      case 2: return <p style={{color: stepCompleted ? 'lightgreen' : 'orange'}} className="message-content">Great! Now Click on the <b>Player Frame</b> on the <b>Bottom Left</b> to <b>Target</b> Yourself.</p>
+      case 3: return <p style={{color: stepCompleted ? 'lightgreen' : 'orange'}} className="message-content">Now you understand <b>Targeting</b>. To cast a spell on a <b>Target</b> you MUST target them first!
+      <hr />
+      Now <b>Target</b> {this.props.paladinName} once more!
+    </p>
       default: return null
     }
   }
@@ -71,34 +80,57 @@ export default class CombatTutorial extends Component {
   }
   renderPaladin() {
     const {paladinName} = this.props
-    const {hp, initHp, mana, power, manaRegen, target} = this.state
-
-    return <div className={`column is-6 is-offset-3 button MemberFrame ${target == paladinName ? 'is-success' : ''}`} style={{height: '25vh'}} onClick={() => this.targetPaladin()}>
-      <div className="columns has-text-centered">
-        <div className="column is-6">
-          <h1 className={`subtitle is-1`} style={{color: 'black'}}>
-            {paladinName}
-            <ClassIcon id={`Recruit-1`} heroClass={'Paladin'} />
-          </h1>
+    const {hp, initHp, mana, power, manaRegen, target, step} = this.state
+    return <span class="has-text-centered">
+      <div className={`column is-6 is-offset-3 button MemberFrame ${target == paladinName ? 'is-success' : ''}`} style={{height: '25vh', borderColor: step == 1 ? 'red' : 'black'}} onClick={() => this.targetPaladin()}>
+        <div className="columns has-text-centered">
+          <div className="column is-6">
+            <h1 className={`subtitle is-1`} style={{color: 'black'}}>
+              {paladinName}
+              <Tooltip
+                animation="scale"
+                arrow="true"
+                position="top"
+                intertia="true"
+                size="big"
+                theme="dark"
+                open={step == 1 || step == 3}
+                html={this.messageSwitch()}
+                class="has-text-centered"
+              />
+              <ClassIcon id={`Recruit-1`} heroClass={'Paladin'} />
+            </h1>
+          </div>
         </div>
+        <br />
+        {this.renderHealthBar()}
       </div>
-      <br />
-      {this.renderHealthBar()}
-    </div>
+    </span>
   }
   renderPlayer() {
-    const {mana, power, maxMana, manaRegen, target} = this.state
+    const {mana, power, maxMana, manaRegen, target, step} = this.state
     const hp = 100
     const initHp = 100
     return <div className="section PlayerFrame">
       <div className="columns is-mobile">
         <div className="column is-4">
           <div
-            style={{cursor: 'pointer', backgroundColor: target == 'Player' ? 'lightgreen' : 'white'}}
+            style={{cursor: 'pointer', backgroundColor: target == 'Player' ? 'lightgreen' : 'white', borderColor: step == 2 ? 'red' : 'black'}}
             className="PlayerBox box"
             onClick={() => this.targetPlayer()}>
             <div className="level">
               <p className="subtitle is-1"><PowerIcon value={Math.floor(power * 10) / 10} /></p>
+              <Tooltip
+                animation="scale"
+                arrow="true"
+                position="top"
+                intertia="true"
+                size="big"
+                theme="dark"
+                open={step == 2}
+                html={this.messageSwitch()}
+                class="has-text-centered"
+              />
               <p className="subtitle is-1"><PlayerIcon player={{name: 'Player'}} />&nbsp;&nbsp;&nbsp;</p>
             </div>
             <div className="columns is-mobile">
@@ -119,11 +151,11 @@ export default class CombatTutorial extends Component {
   }
   render() {
     const {paladinName} = this.props
-    const {hp, initHp, mana, power, manaRegen, target} = this.state
+    const {hp, initHp, mana, power, manaRegen, target, stepCompleted} = this.state
     return <div>
-      {this.renderTutorialMessage()}
       {this.renderPaladin()}
       {this.renderPlayer()}
+      {this.renderTutorialMessage()}
     </div>
   }
 }
