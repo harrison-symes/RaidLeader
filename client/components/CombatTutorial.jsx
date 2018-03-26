ba3e6e600d94312c3ac5cce26ef298c1fc26adc4
harrison-symes <symeshjb@gmail.com>
2018-03-26 18:40:00 +1300

    Cast steps working, but there is some interval duplicate bug

--- client/components/CombatTutorial.jsx
+++ client/components/CombatTutorial.jsx
@@ -7,6 +7,8 @@ import ManaBar from './frames/ManaBar'
 
 import { Line, Circle } from 'rc-progress';
 import {Tooltip} from 'react-tippy'
+import { Progress } from 'react-sweet-progress';
+
 
 export default class CombatTutorial extends Component {
   constructor(props) {
@@ -19,17 +21,17 @@ export default class CombatTutorial extends Component {
       power: 20,
       manaRegen: 0,
       target: null,
-      step: 1,
-      spellCast: 2.5,
-      coolDown: 2,
+      step: 11,
+      spellCast: 5,
+      coolDown: 5,
       cost: 5,
       currentCast: 0,
       currentCooldown: 0,
       onCooldown: false,
       stepCompleted: false
     }
-    this.castInterval = false
-    this.cooldownInterval = false
+    this.castTimer = null
+    this.coolDownTimer = null
   }
   toggleCompletion() {
     this.setState({stepCompleted: !this.state.stepCompleted})
@@ -47,6 +49,53 @@ export default class CombatTutorial extends Component {
     if (this.state.step == 2) this.completeStep(2)
     this.setState({target: 'Player'})
   }
+  tickCD() {
+    let currentCooldown = this.state.currentCooldown + 0.1
+    console.log("tick CD", currentCooldown);
+    if (this.state.onCooldown && currentCooldown >= this.state.coolDown) {
+      console.log("CD finished");
+      window.clearTimeout(this.coolDownTimer)
+      if (this.state.step <= 14) {
+        if (this.state.mana == 0) this.setState({step: 16})
+        else this.setState({step: 15})
+      }
+      return this.setState({onCooldown: false, currentCooldown: 0, currentCast: 0})
+    }
+    else {
+      this.setState({currentCooldown})
+      this.coolDownTimer = setTimeout(() => this.tickCD(), 100)
+    }
+  }
+  finishCast() {
+    let {hp, initHp, power} = this.state
+    hp += power
+    if (hp >= initHp) hp = initHp
+    this.setState({onCooldown: true, hp, mana: this.state.mana -5 <= 0 ? 0 : this.state.mana - 5})
+    if (this.state.step == 13) this.setState({step: 14})
+    this.tickCD()
+  }
+  tickCast() {
+    let currentCast = this.state.currentCast + 0.1
+    if (currentCast >= this.state.spellCast) {
+      if (this.state.step == 12) {
+        window.clearTimeout(this.castTimer)
+        this.setState({step: 13})
+        return setTimeout(() => this.finishCast(), 4000)
+      }
+      else return this.finishCast()
+    } else {
+      this.setState({currentCast})
+      this.castTimer = setTimeout(() => this.tickCast(), 100)
+    }
+  }
+  castHeal() {
+    if (this.state.step == 10) this.setState({step: 11})
+    else if (this.state.step == 11) this.setState({step: 12})
+    else if (this.state.step == 15) this.setState({step: 13})
+    if (this.state.target == this.props.paladinName && this.state.step >= 10 && this.state.mana >= 5) {
+      this.tickCast()
+    }
+  }
   messageSwitch() {
     const {step, stepCompleted} = this.state
     switch(step) {
@@ -62,6 +111,17 @@ export default class CombatTutorial extends Component {
       case 7: return <p style={{color: stepCompleted ? 'lightgreen' : 'orange'}} className="message-content">This bar represents you <b>Player Health</b>. If this bar runs out, its <b>Game Over</b></p>
       case 8: return <p style={{color: stepCompleted ? 'lightgreen' : 'orange'}} className="message-content">This bar represents you <b>Player Mana</b>. Casting <b>Spells</b> Costs <b>Mana</b>. If you don't have enough Mana, you can't cast the <b>Spell</b>.</p>
       case 9: return <p style={{color: stepCompleted ? 'lightgreen' : 'orange'}} className="message-content">If you hover over this Icon, you can see more information about your <b>Player</b>. (Such as <b>Mana Regen</b>)</p>
+      case 10: return <p style={{color: stepCompleted ? 'lightgreen' : 'orange'}} className="message-content">This is a <b>Spell</b>. Specifically a Spell called
+        <b>Heal</b>.
+        <br/>
+        <b>Heal</b> restores <b>Health</b> to your target equal to <b>100% Player Power</b>, so it will heal for <b>20</b> in this case
+      </p>
+      case 11: return <p style={{color: stepCompleted ? 'lightgreen' : 'orange'}} className="message-content">Now! To <b>Heal</b> your friend <b>{this.props.paladinName}</b>, <b>Target</b> them and <b>Click</b> this Spell!</p>
+      case 12: return <p style={{color: stepCompleted ? 'lightgreen' : 'orange'}} className="message-content">This <b>Blue Bar</b> means the spell is casting</p>
+      case 13: return <p style={{color: stepCompleted ? 'lightgreen' : 'orange'}} className="message-content">When the <b>Blue Bar</b> is full, the <b>Spell</b> will Cast!</p>
+      case 14: return <p style={{color: stepCompleted ? 'lightgreen' : 'orange'}} className="message-content">After a <b>Spell</b> is Cast, it must <b>Cool Down</b> for a period of time before it can be used again.</p>
+      case 15: return <p style={{color: stepCompleted ? 'lightgreen' : 'orange'}} className="message-content">Your Spell has Finished its <b>Cooldown</b> and is ready to Cast Again</p>
+      case 16: return <p style={{color: stepCompleted ? 'lightgreen' : 'orange'}} className="message-content">You have run out of <b>Mana</b> and must wait for it to regenerate before you can cast another <b>Spell</b></p>
     }
   }
   renderTutorialMessage() {
@@ -148,38 +208,61 @@ export default class CombatTutorial extends Component {
     </span>
   }
   renderPlayerSpell() {
-    const {mana, target, cost, spellCast, currentCD, currentCast, onCooldown, coolDown} = this.state
+    const {step, mana, target, cost, spellCast, currentCooldown, currentCast, onCooldown, coolDown} = this.state
 
-    const spellColour = onCooldown || mana < cost ? 'is-danger' : 'is-success'
-    var cdPercentage = (coolDown - currentCD) / coolDown * 100
+    const spellColour = onCooldown || mana < cost || step <= 10 ? 'is-danger' : 'is-success'
+    var cdPercentage = (coolDown - currentCooldown) / coolDown * 100
     var castPercentage = currentCast / spellCast * 100
     let perc = onCooldown ? cdPercentage : castPercentage
     var text = Math.round(perc * (onCooldown ? coolDown: spellCast) / 100)
     let width = 1000 / 1
     if (width > 200) width = 200
     return <div className="columns is-multiline is-mobile PlayerSpellBarContainer has-text-centered">
-      <button
-        className={`PlayerSpell ${spellColour}`}
-        style={{position: 'relative', width, height: width}}
+      <Tooltip
+        animation="scale"
+        arrow="true"
+        position={step == 10 ? 'left' : 'right'}
+        intertia="true"
+        size="big"
+        theme="dark"
+        open={step == 10 || step == 11 || step == 15}
+        html={this.messageSwitch()}
       >
-        {(onCooldown || currentCast > 0)
-        ? <span style={{position: 'relative', width, height: width}} className="CastProgress">
-          <Progress
-            type="circle"
-            percent={Math.round(perc)}
-            width={width * 0.9}
-            symbolClassName={`ra ra-sword`}
-            status={onCooldown ? 'danger' : 'casting'}
-            strokeWidth={10}
-            theme={{
-              casting: {symbol: null, color: 'blue'},
-              danger: {symbol: null, color: 'red'}
-            }}
-          />
-        </span>
-        : <i onClick={() => this.clickSpell()} style={{position: 'relative', color: 'green', backgroundColor:'white', width: '90%', height: '90%', margin: 'auto'}} className={`ra ra-5x ra-sword icon icon-large`}
-        />}
-      </button>
+        <button
+          className={`PlayerSpell button ${spellColour}`}
+          style={{position: 'relative', width, height: width}}
+          onClick={() => this.castHeal()}
+        >
+          {(onCooldown || currentCast > 0)
+          ? <span style={{position: 'relative', width, height: width}} className="CastProgress">
+            <Tooltip
+              animation="scale"
+              arrow="true"
+              position="left"
+              intertia="true"
+              size="big"
+              theme="dark"
+              open={step == 12 || step == 13 || step == 14}
+              html={this.messageSwitch()}
+            >
+              <Progress
+                type="circle"
+                percent={Math.round(perc)}
+                width={width * 0.9}
+                symbolClassName={`ra ra-sword`}
+                status={onCooldown ? 'danger' : 'casting'}
+                strokeWidth={10}
+                theme={{
+                  casting: {symbol: null, color: 'blue'},
+                  danger: {symbol: null, color: 'red'}
+                }}
+              />
+            </Tooltip>
+          </span>
+          : <i onClick={() => this.castHeal()} style={{position: 'relative', color: 'green', backgroundColor:'white', width: '90%', height: '90%', margin: 'auto'}} className={`ra ra-5x ra-sword icon icon-large`}
+          />}
+        </button>
+      </Tooltip>
     </div>
   }
   renderPlayer() {
@@ -257,7 +340,7 @@ export default class CombatTutorial extends Component {
                     position="top"
                     intertia="true"
                     theme="dark"
-                    open={step == 8}
+                    open={step == 8 || step == 16}
                     html={this.messageSwitch()}
                     class="has-text-centered"
                   >
