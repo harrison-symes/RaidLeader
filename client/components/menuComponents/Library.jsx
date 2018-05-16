4629ba49001d3be70f4cc03fb924b0f90008a883
harrison-symes <symeshjb@gmail.com>
2018-05-17 10:38:45 +1200

    Library partially refactored

--- client/components/menuComponents/Library.jsx
+++ client/components/menuComponents/Library.jsx
@@ -17,48 +17,37 @@ class Library extends Component {
       learntSpell: null,
       isLoading: false
     }
-    this.showOptions = this.showOptions.bind(this)
-    this.reset = this.reset.bind(this)
   }
-  findByElement(element) {
-    const spellsByElement = Object.keys(spells).filter(spell => !this.props.spellBook.find(learned => learned.name == spell)).map(name => spells[name]).filter(spell => spell.element == element && !spell.reserved)
-    if (spellsByElement.length) return spellsByElement[Math.floor(Math.random() * spellsByElement.length)]
-  }
-  solveOptions() {
-    const offeredSpells = []
-    const spellNames = Object.keys(spells).filter(spell => !this.props.spellBook.find(learned => learned.name == spell))
-    if (spellNames.length < 4) return spellNames.map(name => spells[name])
-    const Life = this.findByElement('Life')
-    const Fire = this.findByElement('Fire')
-    const Shadow = this.findByElement('Shadow')
-    const Arcane = this.findByElement('Arcane')
-    return [Life, Fire, Shadow, Arcane].filter(item => !!item)
 
-    return offeredSpells
+  findByElement = element => {
+    const spellsByElement = Object.keys(spells)
+      .filter(spell => !this.props.spellBook
+        .find(learned => learned.name == spell)).map(name => spells[name])
+      .filter(spell => spell.element == element && !spell.reserved)
+
+    return spellsByElement.length
+      ? spellsByElement[Math.floor(Math.random() * spellsByElement.length)]
+      : null
   }
-  selectSpell (selectedSpell) {
-    this.setState({selectedSpell})
+
+  solveOptions = () => {
+    const elements = ['Life', 'Fire', 'Shadow', 'Arcane']
+
+    return elements
+      .map(element => this.findByElement(element))
+      .filter(item => !!item)
   }
-  reRoll() {
+
+  selectSpell = selectedSpell => this.setState({selectedSpell})
+
+  reRoll = () => {
     if (this.state.isLoading) return
+    this.setState({
+      isLoading: true
+    })
     const rollCost = this.props.spellBook.length * 50 * -1
-    this.setState({isLoading: true})
-    this.props.dispatch(earnGold(rollCost, success => {
-      const offeredSpells = this.solveOptions()
-      set('offeredSpells', JSON.stringify(offeredSpells))
-      this.setState({
-        showChoices: true,
-        offeredSpells,
-        isLoading: false
-      })
-    }))
-  }
-  showOptions() {
-    if (this.state.isLoading) return
 
-    this.setState({isLoading: true})
-    const spellCost = 200 + (this.props.spellBook.length * 50) * -1
-    this.props.dispatch(earnGold(spellCost, success => {
+    this.props.spendGold(rollCost, () => {
       const offeredSpells = this.solveOptions()
       set('offeredSpells', JSON.stringify(offeredSpells))
       this.setState({
@@ -66,13 +55,33 @@ class Library extends Component {
         offeredSpells,
         isLoading: false
       })
-    }))
+    })
   }
-  learnSpell(spell) {
+
+  showOptions = () => {
     if (this.state.isLoading) return
-    this.setState({isLoading: true})
-    
-    this.props.dispatch(addSpell(spell, success => {
+    this.setState({
+      isLoading: true
+    })
+
+    const spellCost = 200 + (this.props.spellBook.length * 50) * -1
+    this.props.spendGold(spellCost, () => {
+      const offeredSpells = this.solveOptions()
+      set('offeredSpells', JSON.stringify(offeredSpells))
+      this.setState({
+        showChoices: true,
+        offeredSpells,
+        isLoading: false
+      })
+    })
+  }
+
+  learnSpell = spell => {
+    if (this.state.isLoading) return
+    this.setState({
+      isLoading: true
+    })
+    this.props.addSpell(spell, () => {
       set('offeredSpells', null)
       this.setState({
         offeredSpells: [],
@@ -81,9 +90,10 @@ class Library extends Component {
         learntSpell: spell,
         isLoading: false
       })
-    }))
+    })
   }
-  reset() {
+
+  reset = () => {
     this.setState({
       offeredSpells: [],
       showChoices: null,
@@ -91,14 +101,115 @@ class Library extends Component {
       learntSpell: null
     })
   }
-  render() {
+
+  renderSpellOption = (spell, i) => {
+    const {selectedSpell} = this.state
+    return <div key={`offered-spell-${i}`} className="box">
+      <div className="level">
+        <p className="title is-3">{spell.name}</p>
+        <span>
+          <SpellIcon spell={spell} isLarge={true} />
+          &nbsp;
+          &nbsp;
+          &nbsp;
+          <button
+            className={`button Info-Button ${selectedSpell != spell ? 'is-success' : 'is-warning'}`}
+            onClick={() => this.selectSpell(selectedSpell != spell
+              ? spell
+              : null
+            )}
+          >
+            {selectedSpell != spell
+              ? 'Details'
+              : 'Hide'
+            }
+          </button>
+        </span>
+      </div>
+      {selectedSpell == spell && <div className="has-text-centered">
+        <div className="subtitle is-5">{spell.description}</div>
+        <div className="columns">
+          <div className="column is-6">
+            <p className="subtitle is-2"><TargetTypeIcon singleTarget={spell.singleTarget}/></p>
+          </div>
+          <div className="column is-6">
+            <p className="subtitle is-2"><SpellElementIcon element={spell.element}/></p>
+          </div>
+        </div>
+        <div className="columns">
+          <div className="column is-4"><p className="subtitle is-4"><ManaIcon value={spell.cost} /></p></div>
+          <div className="column is-4"><p className="subtitle is-4"><CastTimeIcon value={spell.cast + 's'} /></p></div>
+          <div className="column is-4"><p className="subtitle is-4"><CoolDownIcon value={spell.coolDown + 's'} /></p></div>
+        </div>
+        <button onClick={() => this.learnSpell(spell)} className="button is-success is-large">Learn {spell.name}
+          &nbsp;<i className={`icon ra ${spell.icon}`} /> </button>
+        </div>}
+        <hr />
+      </div>
+  }
+
+  renderOptions = () => {
+    const {offeredSpells, showChoices, selectedSpell, learntSpell} = this.state
+    const {spellBook, gold} = this.props
+    const rollCost = spellBook.length * 50
+    const spellCost = 200 + (spellBook.length * 50)
+    const logThing = () => console.log
+    return showChoices
+      ? (
+        <div>
+          <p className="title is-3">Choose a Spell:</p>
+          <span className="subtitle is-3">Or <button className="button is-warning Info-Button" onClick={this.reRoll}>Re-Roll</button> (<GoldIcon value={-1 * rollCost} />)</span>
+          <hr />
+          {offeredSpells.map(this.renderSpellOption)}
+        </div>)
+      : Object.keys(spells)
+        .filter(spell => !this.props.spellBook
+          .find(learned => learned.name == spell)
+        ).length == 0
+        ? <button disabled className="is-danger is-large button is-fullwidth">You have learned every spell!</button>
+        : this.props.spellBook.length <= 2 && this.props.recruits.length < 2
+          ? <button disabled className="is-danger is-large button is-fullwidth">Recruit another Party Member First</button>
+          : gold >= spellCost
+            ? <button onClick={this.showOptions} className="button is-large is-fullwidth">Learn a Spell! (<GoldIcon value={-1 * spellCost} />)</button>
+            : <button className="is-danger is-large button is-fullwidth" disabled><GoldIcon value={`Not Enough`} /></button>
+  }
+
+  learnedSpellFrame = (spell) => {
+    return <div className="has-text-centered">
+      <p className="title is-3">You Learned a new Spell</p>
+      <div className="box">
+        <p className="title is-3">{spell.name}
+        </p>
+        &nbsp;<SpellIcon spell={spell} isLarge={true}/>
+        <p className="subtitle is-5">{spell.description}</p>
+        <div className="columns">
+          <div className="column is-6">
+            <p className="subtitle is-2"><TargetTypeIcon singleTarget={spell.singleTarget}/></p>
+          </div>
+          <div className="column is-6">
+            <p className="subtitle is-2"><SpellElementIcon element={spell.element}/></p>
+          </div>
+        </div>
+        <div className="columns">
+          <div className="column is-4"><p className="subtitle is-4"><ManaIcon value={spell.cost} /></p></div>
+          <div className="column is-4"><p className="subtitle is-4"><CastTimeIcon value={spell.cast + 's'} /></p></div>
+          <div className="column is-4"><p className="subtitle is-4"><CoolDownIcon value={spell.coolDown + 's'} /></p></div>
+        </div>
+      </div>
+      <button onClick={this.reset} className="button is-large is-fullwidth is-info">Learn Another?</button>
+    </div>
+  }
+
+  render = () => {
     const {close, gold, spellBook} = this.props
     const {offeredSpells, showChoices, selectedSpell, learntSpell} = this.state
-    const spellCost = 200 + (spellBook.length * 50)
     const rollCost = this.props.spellBook.length * 50
+    const spellCost = 200 + (spellBook.length * 50)
+
     return <div className="Modal modal is-active">
       <div className="modal-background"></div>
       <div className="modal-card">
+
         <header className="modal-card-head">
           <span className="modal-card-title is-6 level">
             <p className=" is-pulled-left">Library</p>
@@ -108,31 +219,10 @@ class Library extends Component {
             </p>
           </span>
         </header>
+
         <section className="modal-card-body">
           {learntSpell != null
-            ? <div className="has-text-centered">
-              <p className="title is-3">You Learned a new Spell</p>
-              <div className="box">
-                <p className="title is-3">{learntSpell.name}
-                </p>
-                &nbsp;<SpellIcon spell={learntSpell} isLarge={true}/>
-                <p className="subtitle is-5">{learntSpell.description}</p>
-                <div className="columns">
-                  <div className="column is-6">
-                    <p className="subtitle is-2"><TargetTypeIcon singleTarget={learntSpell.singleTarget}/></p>
-                  </div>
-                  <div className="column is-6">
-                    <p className="subtitle is-2"><SpellElementIcon element={learntSpell.element}/></p>
-                  </div>
-                </div>
-                <div className="columns">
-                  <div className="column is-4"><p className="subtitle is-4"><ManaIcon value={learntSpell.cost} /></p></div>
-                  <div className="column is-4"><p className="subtitle is-4"><CastTimeIcon value={learntSpell.cast + 's'} /></p></div>
-                  <div className="column is-4"><p className="subtitle is-4"><CoolDownIcon value={learntSpell.coolDown + 's'} /></p></div>
-                </div>
-              </div>
-              <button onClick={this.reset} className="button is-large is-fullwidth is-info">Learn Another?</button>
-            </div>
+            ? this.learnedSpellFrame(learntSpell)
             : <div>
               <p className="title is-3">Welcome to The Library!</p>
               <p className="content is-large">Here you can learn new spells to support your party in Dungeons</p>
@@ -141,59 +231,11 @@ class Library extends Component {
                 : <p className="title is-3">Thank you for donating to the library, please pick one of these {offeredSpells.length} spells</p>
               }
               <hr />
-              {showChoices
-                ? (<div>
-                  <p className="title is-3">Choose a Spell:</p>
-                  <span className="subtitle is-3">Or <button className="button is-warning Info-Button" onClick={this.reRoll.bind(this)}>Re-Roll</button> (<GoldIcon value={-1 * rollCost} />)</span>
-                  <hr />
-                  {offeredSpells.map((spell, i) => <div key={`offered-spell-${i}`} className="box">
-                    <div className="level">
-                      <p className="title is-3">{spell.name}
-                      </p>
-                      <span>
-                        <SpellIcon spell={spell} isLarge={true} />
-                        &nbsp;
-                        &nbsp;
-                        &nbsp;
-                        {selectedSpell != spell
-                          ? <button onClick={() => this.selectSpell(spell)} className="button Info-Button is-success">Details</button>
-                          : <button onClick={() => this.selectSpell(null)} className="button Info-Button is-warning">Hide</button>
-                        }
-                      </span>
-                    </div>
-                    {selectedSpell == spell && <div className="has-text-centered">
-                      <div className="subtitle is-5">{spell.description}</div>
-                      <div className="columns">
-                        <div className="column is-6">
-                          <p className="subtitle is-2"><TargetTypeIcon singleTarget={spell.singleTarget}/></p>
-                        </div>
-                        <div className="column is-6">
-                          <p className="subtitle is-2"><SpellElementIcon element={spell.element}/></p>
-                        </div>
-                      </div>
-                      <div className="columns">
-                        <div className="column is-4"><p className="subtitle is-4"><ManaIcon value={spell.cost} /></p></div>
-                        <div className="column is-4"><p className="subtitle is-4"><CastTimeIcon value={spell.cast + 's'} /></p></div>
-                        <div className="column is-4"><p className="subtitle is-4"><CoolDownIcon value={spell.coolDown + 's'} /></p></div>
-                      </div>
-                      <button onClick={() => this.learnSpell(spell)} className="button is-success is-large">Learn {spell.name}
-                        &nbsp;<i className={`icon ra ${spell.icon}`} /> </button>
-                      </div>}
-                      <hr />
-                    </div>)}
-                  </div>)
-                  : Object.keys(spells).filter(spell => !this.props.spellBook.find(learned => learned.name == spell)).length != 0
-                  ? this.props.spellBook.length <= 2 && this.props.recruits.length < 2
-                    ? <button disabled className="is-danger is-large button is-fullwidth">Recruit another Party Member First</button>
-                    : gold >= spellCost
-                      ? <button onClick={this.showOptions} className="button is-large is-fullwidth">Learn a Spell! (<GoldIcon value={-1 * spellCost} />)</button>
-                      : <button className="is-danger is-large button is-fullwidth" disabled><GoldIcon value={`Not Enough`} /></button>
-                  : <button disabled className="is-danger is-large button is-fullwidth">You have learned every spell!</button>
-                }
-
-          </div>
+              {this.renderOptions()}
+            </div>
           }
         </section>
+
         <footer className="modal-card-foot">
           <a onClick={close} className="button is-large is-dark is-outlined is-fullwidth">
             <span>Leave&nbsp;</span>
@@ -202,17 +244,25 @@ class Library extends Component {
             </span>
           </a>
         </footer>
+
       </div>
     </div>
   }
 }
 
-const mapStateToProps = ({gold, spellBook, recruits}) => {
-  return {
-    gold,
-    spellBook,
-    recruits
-  }
-}
+const mapDispatchToProps = dispatch => ({
+  addSpell: (spell, cb) => dispatch(addSpell(spell, cb)),
+  spendGold: (cost, cb) => dispatch(earnGold(cost, cb))
+})
 
-export default connect(mapStateToProps)(Library)
+const mapStateToProps = ({
+  gold,
+  spellBook,
+  recruits
+}) => ({
+  gold,
+  spellBook,
+  recruits
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Library)
